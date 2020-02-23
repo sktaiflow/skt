d7ea4536a82db607355638b32fa3f4b9107abd68
igjeong <onevirus@gmail.com>
2020-02-24 05:30:50 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="skt",
+    version="0.0.0",
+    author="SKT",
+    author_email="all@sktai.io",
+    description="SKT package",
+    url="https://github.com/sktaiflow/skt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
