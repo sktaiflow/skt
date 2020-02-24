d686f191b5ebf2fc25ab16596218a4f136bec8ba
igjeong <igjeong@gmail.com>
2020-02-24 11:38:00 +0900

    Fix typo

--- skt/vault_utils.py
+++ skt/vault_utils.py
@@ -1,7 +1,7 @@
 import hvac
 
 
-def get_secret(path, parse_data=True):
+def get_secrets(path, parse_data=True):
     vault_client = hvac.Client()
     data = vault_client.secrets.kv.v2.read_secret_version(path=path)
     if parse_data:
