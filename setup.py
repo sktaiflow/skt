9571ed4a2354d1bf59b8e16c9dc82652e4e7ce67
igjeong <igjeong@gmail.com>
2020-02-24 11:39:49 +0900

    Set version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="skt",
-    version="0.0.2",
+    version="0.1.0",
     author="SKT",
     author_email="all@sktai.io",
     description="SKT package",
