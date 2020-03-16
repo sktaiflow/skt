6690c44e6e5f6973e6fb433381d55a9992d3106c
WooYoung94 <ddsksw01@gmail.com>
2020-03-16 21:24:05 +0900

    flake fix

--- skt/model/utils.py
+++ skt/model/utils.py
@@ -22,7 +22,7 @@ def context_mapping(
     and impression_yn = 'Y'
     """
     post_filter_with_y = hive_to_pandas(query)
-    print('------------------'+'predict_post_filter'+' WITH "Y"'+' loaded')
+    print('------------------' + 'predict_post_filter' + ' WITH "Y"' + ' loaded')
 
     # 2. context_meta Table LOAD
     query = f"""
@@ -30,19 +30,19 @@ def context_mapping(
     where item_id = '{post_filter_with_y['prod_id'][0]}'
     """
     meta = hive_to_pandas(query)
-    print('------------------'+'meta_table'+' WITH "ITEM_ID (PROD_ID)"'+' loaded')
+    print('------------------' + 'meta_table' + ' WITH "ITEM_ID (PROD_ID)"' + ' loaded')
 
     CONTEXT_NUM = meta.shape[0]
     CONTEXT_ID_DEFAULT = meta[meta['context_priority'] == 1]['context_id'].values[0]
     CONTEXT_ID_LIST = tuple(meta['context_id'].values)
-    
+
     # 3. default_setting
     post_filter_with_y['context_id'] = CONTEXT_ID_DEFAULT
-    print('------------------'+'CONTEXT_NUM : 1 (DEFAULT)')
+    print('------------------' + 'CONTEXT_NUM : 1 (DEFAULT)')
 
     if CONTEXT_NUM == 1:
         context_count_list = list(Counter(list(post_filter_with_y['context_id'].values)).items())
-        msg = ('[CONTEXT-MAPPING-SUMMARY]'+'\n'+str(context_count_list[0][0])+' : '+str(context_count_list[0][1]))
+        msg = ('[CONTEXT-MAPPING-SUMMARY]' + '\n'+str(context_count_list[0][0]) +' : '+ str(context_count_list[0][1]))
 
     # 4. context_num >= 2 : context mapping (with context priority)
     else:
@@ -56,9 +56,9 @@ def context_mapping(
         df_context = hive_to_pandas(query)
 
         # 4+a. for loop - overwrite mapping
-        for i in range(CONTEXT_NUM-1):
-            priority_num = i+2
-            print('------------------'+'CONTEXT_NUM : '+str(priority_num))
+        for i in range(CONTEXT_NUM - 1):
+            priority_num = i + 2
+            print('------------------' + 'CONTEXT_NUM : ' + str(priority_num))
             PRIORITY_CONTEXT_ID = meta[meta['context_priority'] == priority_num]['context_id'].values[0]
             post_filter_with_y.loc[post_filter_with_y['svc_mgmt_num'].isin(list(df_context[df_context['dimension'] == PRIORITY_CONTEXT_ID]['svc_mgmt_num'])), "context_id"] = PRIORITY_CONTEXT_ID
 
@@ -66,11 +66,11 @@ def context_mapping(
         context_count_list = list(Counter(list(post_filter_with_y['context_id'].values)).items())
         msg = '[CONTEXT-MAPPING-SUMMARY]'
         for j in range(CONTEXT_NUM):
-            msg_ = ('\n'+str(context_count_list[j][0])+' : '+str(context_count_list[j][1]))
+            msg_ = ('\n' + str(context_count_list[j][0]) +' : ' +str(context_count_list[j][1]))
             msg += msg_
 
     # 5. Slack Report
     slack_send(text=msg, channel="#rec_modeling_alert", icon_emoji=':mag:')
-                              
+ 
     # 6. return post_filter_with_y_with_context_id Table
     return post_filter_with_y
