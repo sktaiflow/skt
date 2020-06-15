60de87842a416782b662a8ddc2e68802a7a2c276
Taekyung Yoo <tkyoo@sktai.io>
2020-06-15 10:10:19 +0900

    apply flake8

--- skt/data_catalog.py
+++ skt/data_catalog.py
@@ -56,34 +56,34 @@ def get_query_detail(source, query_id):
     return requests.get(f"{secrets['url_prd']}/sources/{source}/processes/{query_id}").json()
 
 
-def search_table_by_name(name, **kwargs): 
+def search_table_by_name(name, **kwargs):
     secrets = get_secrets(DATA_CATALOG_SECRETS_NAME)
     kwargs["name"] = name
     return requests.get(f"{secrets['url_prd']}/v1/search/tables", params=kwargs).json()
 
 
-def search_table_by_dc(description, **kwargs): 
+def search_table_by_dc(description, **kwargs):
     secrets = get_secrets(DATA_CATALOG_SECRETS_NAME)
     kwargs["description"] = description
 
     return requests.get(f"{secrets['url_prd']}/v1/search/tables", params=kwargs).json()
 
 
-def search_column_by_dc(description, **kwargs): 
+def search_column_by_dc(description, **kwargs):
     secrets = get_secrets(DATA_CATALOG_SECRETS_NAME)
     kwargs["description"] = description
-    
+
     return requests.get(f"{secrets['url_prd']}/v1/search/columns", params=kwargs).json()
 
 
-def search_column_by_name(name, **kwargs): 
+def search_column_by_name(name, **kwargs):
     secrets = get_secrets(DATA_CATALOG_SECRETS_NAME)
     kwargs["name"] = name
 
     return requests.get(f"{secrets['url_prd']}/v1/search/columns", params=kwargs).json()
 
 
-def search_bigquery_queries_by_table_id(table_id, **kwargs): 
+def search_bigquery_queries_by_table_id(table_id, **kwargs):
     limit = kwargs.get("limit", 100)
     fuzziness = kwargs.get("fuzziness", "AUTO")
     operator = kwargs.get("operator", "and")
@@ -105,17 +105,13 @@ def search_bigquery_queries_by_table_id(table_id, **kwargs):
         "fuzziness": fuzziness,
         "offset": offset,
         "operator": operator,
-        "fields":  fields,
+        "fields": fields,
         "must": must,
-        "sort": json.dumps(es_sort)
+        "sort": json.dumps(es_sort),
     }
 
     if start_date or end_date:
-        range_filter = {
-            "range": {
-                "start_time": { }
-            }
-        }
+        range_filter = {"range": {"start_time": {}}}
 
         if start_date:
             range_filter["range"]["start_time"]["gte"] = start_date
@@ -125,10 +121,10 @@ def search_bigquery_queries_by_table_id(table_id, **kwargs):
 
         params["range_filter"] = json.dumps(range_filter)
 
-    return requests.get(secrets['url_prd'] + "/v1/search/processes", params=params).json()
+    return requests.get(secrets["url_prd"] + "/v1/search/processes", params=params).json()
 
 
-def search_bigquery_queries_by_column(table_id, column_name, **kwargs): 
+def search_bigquery_queries_by_column(table_id, column_name, **kwargs):
     limit = kwargs.get("limit", 100)
     fuzziness = kwargs.get("fuzziness", "AUTO")
     operator = kwargs.get("operator", "and")
@@ -156,13 +152,13 @@ def search_bigquery_queries_by_column(table_id, column_name, **kwargs):
             "fields": "qualified_name",
             "fuzziness": fuzziness,
             "operator": operator,
-            "must": must
+            "must": must,
         }
 
         response = requests.get(catalog_secrets["url_prd"] + "/v1/search/columns", params=params).json()
 
         column_list = response.get("qualified_name", {}).get("hits", [])
-        
+
         if column_list:
             max_score_column_id = column_list[0]["_source"]["qualified_name"]
 
@@ -172,15 +168,20 @@ def search_bigquery_queries_by_column(table_id, column_name, **kwargs):
                 "sort_by_time": True,
                 "order": sort,
                 "start_date": start_date,
-                "end_date": end_date
+                "end_date": end_date,
             }
 
-            relationship_list = requests.get(lineage_secrets["url_prd"] + "/relationships/bigquery/queries/columns/" + max_score_column_id, params=params ).json()
+            relationship_list = requests.get(
+                lineage_secrets["url_prd"] + "/relationships/bigquery/queries/columns/" + max_score_column_id,
+                params=params,
+            ).json()
 
             for each_relationship in relationship_list:
                 query_id = each_relationship["source"]
                 project_id = query_id.split("@")[1]
-                response = requests.get(catalog_secrets["url_prd"] + f"/sources/bigquery-{project_id}/processes/{query_id}").json()
+                response = requests.get(
+                    catalog_secrets["url_prd"] + f"/sources/bigquery-{project_id}/processes/{query_id}"
+                ).json()
 
                 result.append(response)
     else:
@@ -189,7 +190,7 @@ def search_bigquery_queries_by_column(table_id, column_name, **kwargs):
     return result
 
 
-def search_bigquery_queries_by_user_name(user_name, **kwargs): 
+def search_bigquery_queries_by_user_name(user_name, **kwargs):
     limit = kwargs.get("limit", 100)
     fuzziness = kwargs.get("fuzziness", "AUTO")
     operator = kwargs.get("operator", "and")
@@ -210,17 +211,13 @@ def search_bigquery_queries_by_user_name(user_name, **kwargs):
         "fuzziness": fuzziness,
         "offset": offset,
         "operator": operator,
-        "fields":  fields,
+        "fields": fields,
         "must": must,
-        "sort": json.dumps(es_sort)
+        "sort": json.dumps(es_sort),
     }
 
     if start_date or end_date:
-        range_filter = {
-            "range": {
-                "start_time": { }
-            }
-        }
+        range_filter = {"range": {"start_time": {}}}
 
         if start_date:
             range_filter["range"]["start_time"]["gte"] = start_date
@@ -230,7 +227,7 @@ def search_bigquery_queries_by_user_name(user_name, **kwargs):
 
         params["range_filter"] = json.dumps(range_filter)
 
-    return requests.get(secrets['url_prd'] + "/v1/search/processes", params=params).json()
+    return requests.get(secrets["url_prd"] + "/v1/search/processes", params=params).json()
 
 
 def get_user_data_access_in_bigquery(username, start_date, end_date, timeseries=False, **kwargs):
@@ -239,18 +236,10 @@ def get_user_data_access_in_bigquery(username, start_date, end_date, timeseries=
 
     es_sort = [{"start_time": "asc"}]
 
-    params = {
-        "user_name": username,
-        "sort": json.dumps(es_sort),
-        "fields": json.dumps(["inputs","outputs"])
-    }
+    params = {"user_name": username, "sort": json.dumps(es_sort), "fields": json.dumps(["inputs", "outputs"])}
 
     if start_date or end_date:
-        range_filter = {
-            "range": {
-                "start_time": { }
-            }
-        }
+        range_filter = {"range": {"start_time": {}}}
 
         if start_date:
             range_filter["range"]["start_time"]["gte"] = start_date
@@ -259,18 +248,18 @@ def get_user_data_access_in_bigquery(username, start_date, end_date, timeseries=
             range_filter["range"]["start_time"]["lt"] = end_date
 
         params["range_filter"] = json.dumps(range_filter)
-    
+
     total_queries = []
 
-    response = requests.get(secrets['url_prd'] + "/v1/search/processes", params=params).json()
-    
+    response = requests.get(secrets["url_prd"] + "/v1/search/processes", params=params).json()
+
     total_queries.extend(response["user_name"]["hits"])
     total = response["user_name"]["total"]["value"]
 
     while total > len(total_queries):
         params["offset"] = json.dumps(total_queries[-1]["sort"])
 
-        response = requests.get(secrets['url_prd'] + "/v1/search/processes", params=params).json()
+        response = requests.get(secrets["url_prd"] + "/v1/search/processes", params=params).json()
         total_queries.extend(response["user_name"]["hits"])
 
     result = []
@@ -286,32 +275,38 @@ def get_user_data_access_in_bigquery(username, start_date, end_date, timeseries=
             table_list.extend(each_query["_source"]["inputs"])
             table_list.extend(each_query["_source"]["outputs"])
 
-            response = requests.get(lineage_secrets["url_prd"] + f"/relationships/bigquery/queries/query/{query_id}/columns", params=params ).json()
+            response = requests.get(
+                lineage_secrets["url_prd"] + f"/relationships/bigquery/queries/query/{query_id}/columns", params=params
+            ).json()
             column_list = list(map(lambda each: each["target"], response))
 
-            result.append({
-                "inputs": each_query["_source"]["inputs"], 
-                "outputs": each_query["_source"]["outputs"],
-                "columns": column_list,
-                "start_time": each_query["sort"][0]
-            })
+            result.append(
+                {
+                    "inputs": each_query["_source"]["inputs"],
+                    "outputs": each_query["_source"]["outputs"],
+                    "columns": column_list,
+                    "start_time": each_query["sort"][0],
+                }
+            )
         else:
             for each in each_query["_source"]["inputs"]:
                 if each not in table_dict:
                     table_dict[each] = 1
-            
+
             for each in each_query["_source"]["outputs"]:
                 if each not in table_dict:
                     table_dict[each] = 1
 
-            response = requests.get(lineage_secrets["url_prd"] + f"/relationships/bigquery/queries/query/{query_id}/columns", params=params ).json()
+            response = requests.get(
+                lineage_secrets["url_prd"] + f"/relationships/bigquery/queries/query/{query_id}/columns", params=params
+            ).json()
             column_list = list(map(lambda each: each["target"], response))
 
             for each_column in column_list:
                 if each_column not in column_dict:
                     column_dict[each_column] = 1
-    
+
     if timeseries:
         return result
     else:
-        return { "tables": list(table_dict.keys()), "columns": list(column_dict.keys()) }
+        return {"tables": list(table_dict.keys()), "columns": list(column_dict.keys())}
