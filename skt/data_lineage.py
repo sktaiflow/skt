72c31306a50bc38620b424c4a4b22119c56e277b
igjeong <igjeong@gmail.com>
2020-08-14 17:17:39 +0900

    Add publish lineage function

--- skt/data_lineage.py
+++ skt/data_lineage.py
@@ -13,3 +13,18 @@ def get_lineage(table):
 def get_lineage_network(table, sequence=True):
     secrets = get_secrets(DATA_LINEAGE_SECRETS_NAME)
     return requests.get(f"{secrets['url_prd']}/relationships/lineage/node/{table}/network?sequence={sequence}")
+
+
+def publish_relation(source, destination, context=None):
+    from datetime import datetime
+
+    msg = {
+        "source": source,
+        "destination": destination,
+        "timestamp": round(datetime.utcnow().timestamp() * 1000),
+        "context": context,
+    }
+    proxies = get_secrets(path="proxies")
+    url = get_secrets(path="data_lineage")["url"]
+
+    return requests.post(url, proxies=proxies, json=msg)
